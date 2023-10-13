29d4b3b7f6010df9a25e0568efb4e6aba8e1ff4e
LachlanSP <elgameso@protonmail.ch>
2023-10-14 09:38:08 +1100

    Replace terminal with PySimpleGUI interface

--- shutdown.py
+++ shutdown.py
@@ -1,42 +1,64 @@
-# Program that asks for the time to shutdown and forces a shutdown at the specified time
+# Addition to shutdown.py that instead does a shutdown in time from now, rather than hour of day
 import os
-import datetime
+from datetime import date, time
 import time
+import PySimpleGUI as gui
 
-def currentTime():
-    print("Current time: ")
-    print(datetime.datetime.now().strftime('%H:%M:%S on %d/%m/%y'))
-    
-def shutdown():
-    currentTime()
-    currentHour = datetime.datetime.now().time().hour
-    shutdownHour = input("At which hour (in 24hr time) do you wish the computer to turn off\n")
-    shutdownHour = int(shutdownHour)
-    if shutdownHour < currentHour or shutdownHour > 23:
-        print("Invalid hour selection, exiting...")
-        time.sleep(5)
-        exit()
+def getRadioButton(values):
+    # Return the value of the 
+    if values['shutdown']:
+        return 'shutdown'
+    elif values['hibernate']:
+        return 'hibernate'
     else:
-        print("Valid hour selection. The system will shutdown during the " + str(shutdownHour) + " hour")
-        hourDelta = shutdownHour - currentHour
-        print("Hours until shutdown: " + str(hourDelta))
-        print("To cancel the scheduled shutdown, close this window.")
-        time.sleep((hourDelta*3600))
-        if shutdownHour >= currentHour:
-            os.system("shutdown /s /t 15")
+        return 'sleep'
     
-    
-
+def countdown(minutes, action):
+    try:
+        seconds = minutes * 60
+        for remaining in range(seconds, 0, -1):
+            timeString = time.strftime('%H:%M:%S', time.localtime(remaining))
+            gui.popup_no_wait(f"Countdown: {timeString}", title="Time to {action}", non_blocking=True)
+            time.sleep(1)
+        
+        if action == 'shutdown':
+            os.system('shutdown /s /t 0')
+        elif action == 'hibernate':
+            os.system('hibernate /h')
+        elif action == 'sleep':
+            os.system('rundll32.exe powrprof.dll,SetSuspendState 0,1,0')
+    except Exception as error:
+        gui.popup_error(f"An unexpected error occurred: {str(error)}")
 
 def main():
-    print("This program will ask for a time to shutdown your computer")
-    answer = input("1. Continue\n2. Exit\n")
-    if answer == "1":
-        shutdown()
-    elif answer == "2":
-        print("Exiting...")
-        exit()
-    else:
-        print("Input not valid, please enter either 1 or 2")
-    
+
+    layout = [
+        [gui.Text('Countdown Timer')],
+        [gui.InputText(key='time_input', size=(10, 1)), gui.Text('minutes')],
+        [gui.Radio('Shutdown', 'radio_group', key='shutdown', default=True),
+            gui.Radio('Hibernate', 'radio_group', key='hibernate'), 
+            gui.Radio('Sleep', 'radio_group', key='sleep')],
+        [gui.Button('Start Countdown'), gui.Button('Exit')],
+    ]
+
+    window = gui.Window('Shutdown/Hibernate/Sleep Timer', layout)
+
+    while True:
+        event, values = window.read()
+        #Exit if the user closes the window or clicks exit button
+        if event == gui.WINDOW_CLOSED or event == 'Exit':
+            break
+
+        #Start countdown if the countdown button is clicked
+        if event == 'Start Countdown':
+            try:
+                countdown_minutes = int(values['time_input'])
+                action = getRadioButton(values)
+            except ValueError:
+                gui.popup_error("Please enter a valid number of minutes.")
+            except Exception as error:
+                gui.popup_error(f"An unexpected error occurred: {str(error)}")
+
+
 main()
+
